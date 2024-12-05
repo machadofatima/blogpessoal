8bd39bb1116f30b76576f74b8700aa8a5604118f
FatimaMachado <machadofatima@live.com>
2024-12-05 19:51:38 -0300

    feat:  inclusão das classes UserDetailsImpl e UserDetailsServiceImp

--- controller/TemaController.java
+++ controller/TemaController.java
@@ -44,8 +44,7 @@ public class TemaController {
     }
 
     @GetMapping("/descricao/{descricao}")
-    public ResponseEntity<List<Tema>> getByTitle(@PathVariable
-                                                 String descricao){
+    public ResponseEntity<List<Tema>> getByTitle(@PathVariable String descricao){
         return ResponseEntity.ok(temaRepository
                 .findAllByDescricaoContainingIgnoreCase(descricao));
     }
